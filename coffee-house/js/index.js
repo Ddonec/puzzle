9568071abba87fa5f3f14b29b2042046b598b068
Anton <137609245+Ddonec@users.noreply.github.com>
2023-11-26 15:33:20 +0300

    feat: slider home / buttons menu complet

--- coffee-house/js/index.js
+++ coffee-house/js/index.js
@@ -5,35 +5,52 @@ const sliderWidthContainer = document.querySelector(
    ".product-slider-container"
 );
 
+const pag1 = document.querySelector(".pag-1");
+const pag2 = document.querySelector(".pag-2");
+const pag3 = document.querySelector(".pag-3");
+
 const widthSlider = sliderWidthContainer.clientWidth;
 let count = 1;
 
 leftArrow.addEventListener("click", function () {
-   console.log("10");
    if (count == 1) {
       sliderCont.style.transform = "translateX(0px)";
    } else if (count == 2) {
       sliderCont.style.transform = "translateX(0px)";
       count -= 1;
+      pag1.classList.add("pagactive");
+      pag2.classList.remove("pagactive");
+      pag3.classList.remove("pagactive");
    } else if (count == 3) {
       sliderCont.style.transform = "translateX(-" + widthSlider + "px)";
       count -= 1;
+      pag1.classList.remove("pagactive");
+      pag2.classList.add("pagactive");
+      pag3.classList.remove("pagactive");
    }
+   console.log(widthSlider);
    console.log(count);
 });
 
 rightArrow.addEventListener("click", function () {
-   console.log("20");
    if (count == 1) {
       sliderCont.style.transform = "translateX(-" + widthSlider + "px)";
       count += 1;
+      pag1.classList.remove("pagactive");
+      pag2.classList.add("pagactive");
+      pag3.classList.remove("pagactive");
    } else if (count == 2) {
       let widthSlider2 = widthSlider * 2;
       sliderCont.style.transform = "translateX(-" + widthSlider2 + "px)";
       count += 1;
+      pag1.classList.remove("pagactive");
+      pag2.classList.remove("pagactive");
+      pag3.classList.add("pagactive");
    } else if (count == 3) {
-      sliderCont.style.transform = "translateX(-" + widthSlider + "px)";
+      let widthSlider2 = widthSlider * 2;
+      sliderCont.style.transform = "translateX(-" + widthSlider2 + "px)";
    }
 
+   console.log(widthSlider);
    console.log(count);
 });
