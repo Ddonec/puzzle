d2387777883856b43e9904804f8e9e72735c3320
Anton <137609245+Ddonec@users.noreply.github.com>
2023-12-02 23:23:43 +0300

    feat: adaptive 1440 - 768 - 380 all lastik

--- coffee-house/js/filter-btns-menu.js
+++ coffee-house/js/filter-btns-menu.js
@@ -4,29 +4,29 @@ const cakeBtn = document.querySelector(".cake-btn-mebu");
 
 const coffeeCards = document.querySelector(".cards-of-product");
 const teaCards = document.querySelector(".products-tea");
-const cakesCards = document.querySelector(".cakes-product");
+const cakesCards = document.querySelector(".cake-products-container");
 
 coffeeBtn.addEventListener("click", function () {
-   coffeeCards.classList.remove("none");
-   teaCards.classList.add("none");
-   cakesCards.classList.add("none");
-   coffeeBtn.classList.add("active-shop");
-   teaBtn.classList.remove("active-shop");
-   cakeBtn.classList.remove("active-shop");
+    coffeeCards.classList.remove("none");
+    teaCards.classList.add("none");
+    cakesCards.classList.add("none");
+    coffeeBtn.classList.add("active-shop");
+    teaBtn.classList.remove("active-shop");
+    cakeBtn.classList.remove("active-shop");
 });
 teaBtn.addEventListener("click", function () {
-   coffeeCards.classList.add("none");
-   teaCards.classList.remove("none");
-   cakesCards.classList.add("none");
-   coffeeBtn.classList.remove("active-shop");
-   teaBtn.classList.add("active-shop");
-   cakeBtn.classList.remove("active-shop");
+    coffeeCards.classList.add("none");
+    teaCards.classList.remove("none");
+    cakesCards.classList.add("none");
+    coffeeBtn.classList.remove("active-shop");
+    teaBtn.classList.add("active-shop");
+    cakeBtn.classList.remove("active-shop");
 });
 cakeBtn.addEventListener("click", function () {
-   coffeeCards.classList.add("none");
-   teaCards.classList.add("none");
-   cakesCards.classList.remove("none");
-   coffeeBtn.classList.remove("active-shop");
-   teaBtn.classList.remove("active-shop");
-   cakeBtn.classList.add("active-shop");
+    coffeeCards.classList.add("none");
+    teaCards.classList.add("none");
+    cakesCards.classList.remove("none");
+    coffeeBtn.classList.remove("active-shop");
+    teaBtn.classList.remove("active-shop");
+    cakeBtn.classList.add("active-shop");
 });
