a804c4133ed05e4206f725f493e50c7f16bf9d0f
Anton <137609245+Ddonec@users.noreply.github.com>
2023-12-04 16:23:06 +0300

    feat: hover touch device off, links rebuild

--- coffee-house/js/burger.js
+++ coffee-house/js/burger.js
@@ -1,6 +1,7 @@
 const burgerBtn = document.querySelector(".burger-button");
 const burgerMenu = document.querySelector(".burger-menu");
 const links = document.querySelectorAll(".burger-menu li a");
+const menuLink = document.querySelector(".menu-underline a");
 const body = document.querySelector("body");
 
 let burgerStatus = false;
@@ -15,6 +16,9 @@ links.forEach((link) => {
     link.addEventListener("click", function () {
         closeBurgerMenu();
     });
+});
+menuLink.addEventListener("click", function () {
+    closeBurgerMenu();
 });
 
 function toggleBurgerMenu() {
