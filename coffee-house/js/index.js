a49ef36f065af6043e030cd1e4c885d25f706365
Anton <137609245+Ddonec@users.noreply.github.com>
2023-12-05 01:50:06 +0300

    feat: image visible fix, js slider update

--- coffee-house/js/index.js
+++ coffee-house/js/index.js
@@ -1,56 +1,67 @@
 const leftArrow = document.querySelector(".left-arrow");
 const rightArrow = document.querySelector(".right-arrow");
 const sliderCont = document.querySelector(".product-into-slider-container");
-const sliderWidthContainer = document.querySelector(
-   ".product-slider-container"
-);
+const sliderWidthContainer = document.querySelector(".product-slider-container");
 
 const pag1 = document.querySelector(".pag-1");
 const pag2 = document.querySelector(".pag-2");
 const pag3 = document.querySelector(".pag-3");
 
-const widthSlider = sliderWidthContainer.clientWidth;
+let widthSlider = sliderWidthContainer.clientWidth;
 let count = 1;
 
+window.addEventListener("resize", function () {
+    widthSlider = sliderWidthContainer.clientWidth;
+    count = 1;
+    sliderCont.style.transform = "translateX(0px)";
+    pag1Active();
+});
 leftArrow.addEventListener("click", function () {
-   if (count == 1) {
-      sliderCont.style.transform = "translateX(0px)";
-   } else if (count == 2) {
-      sliderCont.style.transform = "translateX(0px)";
-      count -= 1;
-      pag1.classList.add("pagactive");
-      pag2.classList.remove("pagactive");
-      pag3.classList.remove("pagactive");
-   } else if (count == 3) {
-      sliderCont.style.transform = "translateX(-" + widthSlider + "px)";
-      count -= 1;
-      pag1.classList.remove("pagactive");
-      pag2.classList.add("pagactive");
-      pag3.classList.remove("pagactive");
-   }
-   console.log(widthSlider);
-   console.log(count);
+    if (count == 1) {
+        sliderCont.style.transform = "translateX(0px)";
+    } else if (count == 2) {
+        sliderCont.style.transform = "translateX(0px)";
+        count -= 1;
+        pag1Active();
+    } else if (count == 3) {
+        sliderCont.style.transform = "translateX(-" + widthSlider + "px)";
+        count -= 1;
+        pag2Active();
+    }
+    console.log(widthSlider);
+    console.log(count);
 });
 
 rightArrow.addEventListener("click", function () {
-   if (count == 1) {
-      sliderCont.style.transform = "translateX(-" + widthSlider + "px)";
-      count += 1;
-      pag1.classList.remove("pagactive");
-      pag2.classList.add("pagactive");
-      pag3.classList.remove("pagactive");
-   } else if (count == 2) {
-      let widthSlider2 = widthSlider * 2;
-      sliderCont.style.transform = "translateX(-" + widthSlider2 + "px)";
-      count += 1;
-      pag1.classList.remove("pagactive");
-      pag2.classList.remove("pagactive");
-      pag3.classList.add("pagactive");
-   } else if (count == 3) {
-      let widthSlider2 = widthSlider * 2;
-      sliderCont.style.transform = "translateX(-" + widthSlider2 + "px)";
-   }
+    if (count == 1) {
+        sliderCont.style.transform = "translateX(-" + widthSlider + "px)";
+        count += 1;
+        pag2Active();
+    } else if (count == 2) {
+        let widthSlider2 = widthSlider * 2;
+        sliderCont.style.transform = "translateX(-" + widthSlider2 + "px)";
+        count += 1;
+        pag3Active();
+    } else if (count == 3) {
+        let widthSlider2 = widthSlider * 2;
+        sliderCont.style.transform = "translateX(-" + widthSlider2 + "px)";
+    }
 
-   console.log(widthSlider);
-   console.log(count);
+    console.log(widthSlider);
+    console.log(count);
 });
+function pag1Active() {
+    pag1.classList.add("pagactive");
+    pag2.classList.remove("pagactive");
+    pag3.classList.remove("pagactive");
+}
+function pag2Active() {
+    pag1.classList.remove("pagactive");
+    pag2.classList.add("pagactive");
+    pag3.classList.remove("pagactive");
+}
+function pag3Active() {
+    pag1.classList.remove("pagactive");
+    pag2.classList.remove("pagactive");
+    pag3.classList.add("pagactive");
+}
