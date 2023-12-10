0a3563f02703394d13624b7d137a382e66d02d9d
Anton <137609245+Ddonec@users.noreply.github.com>
2023-12-10 23:42:41 +0300

    feat:modal adaptive 380 - 1440 px

--- coffee-house/js/modal.js
+++ coffee-house/js/modal.js
@@ -1,8 +1,15 @@
 const closeBtnModal = document.querySelector(".close-btn-modal");
 const greyBG = document.querySelector(".grey-bg");
 const modalWindow = document.querySelector(".modal-coffe");
+const productCart = document.querySelector(".product-cart");
 
 closeBtnModal.addEventListener("click", function () {
     greyBG.style.display = "none";
     modalWindow.style.display = "none";
+});
+productCart.addEventListener("click", function () {
+    greyBG.classList.remove("none");
+    modalWindow.classList.remove("none");
+    greyBG.style = "";
+    modalWindow.style = "";
 });
