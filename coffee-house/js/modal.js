0b5ac28270592da174dbb6d3a5444449ba3bd00e
Anton <137609245+Ddonec@users.noreply.github.com>
2023-12-11 02:14:29 +0300

    feat: modal script update

--- coffee-house/js/modal.js
+++ coffee-house/js/modal.js
@@ -2,30 +2,28 @@ const closeBtnModal = document.querySelector(".close-btn-modal");
 const greyBG = document.querySelector(".grey-bg");
 const modalWindow = document.querySelector(".modal-coffe");
 const productCart = document.querySelector(".product-cart");
+const products = document.querySelectorAll(".product-cart");
 
 const closeModalElementsArr = [greyBG, closeBtnModal];
+
 closeModalElementsArr.forEach(function (element) {
     element.addEventListener("click", function () {
         closeModal();
     });
 });
-// productCart.addEventListener("click", function () {
-//     openModal();
-// });
 
 function openModaScriptl() {
     greyBG.classList.remove("none");
     modalWindow.classList.remove("none");
     greyBG.style = "";
     modalWindow.style = "";
+    document.querySelector("body").style.overflow = "hidden";
 }
-
 function closeModal() {
     greyBG.style.display = "none";
     modalWindow.style.display = "none";
+    document.querySelector("body").style = "";
 }
-
-const products = document.querySelectorAll(".product-cart");
 
 products.forEach((product) => {
     product.addEventListener("click", () => {
@@ -51,89 +49,16 @@ function openModal(image, title, description, price, cat) {
     const additivesModalBtns = document.querySelector(".third-info-modal .size-value-modal-btns-cont");
 
     if (cat === "coffeeCat") {
-        // Фильтры для кофе
-        sizeValueModalBtns.innerHTML = `
-        <div class="size-value-btn">
-        <div>S</div>
-        <p>200 ml</p>
-    </div>
-    <div class="size-value-btn">
-        <div>M</div>
-        <p>300 ml</p>
-    </div>
-    <div class="size-value-btn">
-        <div>L</div>
-        <p>400 ml</p>
-    </div>`;
-        additivesModalBtns.innerHTML = `
-        <div class="size-value-btn">
-        <div>1</div>
-        <p>Sugar</p>
-    </div>
-    <div class="size-value-btn">
-        <div>2</div>
-        <p>Cinnamon</p>
-    </div>
-    <div class="size-value-btn">
-        <div>3</div>
-        <p>Syrup</p>
-    </div>`;
-    } else if (cat === "teaCat") {
-        // Фильтры для чая
-        sizeValueModalBtns.innerHTML = `
-        <div class="size-value-btn">
-        <div>S</div>
-        <p>200 ml</p>
-    </div>
-    <div class="size-value-btn">
-        <div>M</div>
-        <p>300 ml</p>
-    </div>
-    <div class="size-value-btn">
-        <div>L</div>
-        <p>400 ml</p>
-    </div>`;
-        additivesModalBtns.innerHTML = `
-        <div class="size-value-btn">
-        <div>1</div>
-        <p>Sugar</p>
-    </div>
-    <div class="size-value-btn">
-        <div>2</div>
-        <p>Lemon</p>
-    </div>
-    <div class="size-value-btn">
-        <div>3</div>
-        <p>Syrup</p>
-    </div>`;
-    } else if (cat === "dessertCat") {
-        // Фильтры для десертов
-        sizeValueModalBtns.innerHTML = `
-        <div class="size-value-btn">
-        <div>S</div>
-        <p>50g</p>
-    </div>
-    <div class="size-value-btn">
-        <div>M</div>
-        <p>100g</p>
-    </div>
-    <div class="size-value-btn">
-        <div>L</div>
-        <p>150g</p>
-    </div>`;
-        additivesModalBtns.innerHTML = `
-        <div class="size-value-btn">
-        <div>1</div>
-        <p>Berries</p>
-    </div>
-    <div class="size-value-btn">
-        <div>2</div>
-        <p>Nuts</p>
-    </div>
-    <div class="size-value-btn">
-        <div>3</div>
-        <p>Jam</p>
-    </div>`;
+        sizeValueModalBtns.innerHTML = `<div class="size-value-btn"><div>S</div><p>200 ml</p></div><div class="size-value-btn"><div>M</div><p>300 ml</p></div><div class="size-value-btn"><div>L</div><p>400 ml</p></div>`;
+        additivesModalBtns.innerHTML = `<div class="size-value-btn"><div>1</div><p>Sugar</p></div><div class="size-value-btn"><div>2</div><p>Cinnamon</p></div><div class="size-value-btn"><div>3</div><p>Syrup</p></div>`;
+    }
+    if (cat === "teaCat") {
+        sizeValueModalBtns.innerHTML = `<div class="size-value-btn"><div>S</div><p>200 ml</p></div><div class="size-value-btn"><div>M</div><p>300 ml</p></div><div class="size-value-btn"><div>L</div><p>400 ml</p></div>`;
+        additivesModalBtns.innerHTML = `<div class="size-value-btn"><div>1</div><p>Sugar</p></div><div class="size-value-btn"><div>2</div><p>Lemon</p></div><div class="size-value-btn"><div>3</div><p>Syrup</p></div>`;
+    }
+    if (cat === "dessertCat") {
+        sizeValueModalBtns.innerHTML = `<div class="size-value-btn"><div>S</div><p>50 g</p></div><div class="size-value-btn"><div>M</div><p>100 g</p></div><div class="size-value-btn"><div>L</div><p>200 g</p></div>`;
+        additivesModalBtns.innerHTML = `<div class="size-value-btn"><div>1</div><p>Berries</p></div><div class="size-value-btn"><div>2</div><p>Nuts</p></div><div class="size-value-btn"><div>3</div><p>Jam</p></div>`;
     }
     modalImage.src = image;
     modalTitle.textContent = title;
@@ -146,5 +71,3 @@ function openModal(image, title, description, price, cat) {
     console.log(price);
     console.log(cat);
 }
-
-function updateModalButtonCat() {}
