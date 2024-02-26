dae2a3222bd254e30c4a69eec53291b984cacd52
Anton <137609245+Ddonec@users.noreply.github.com>
2024-02-26 14:20:16 +0300

    feat: add feature (theme switcher) use .ts

---
+++ news-JS-main/src/components/view/slider.ts
@@ -0,0 +1,12 @@
+export function setTheme(themeName: string): void {
+    localStorage.setItem('theme', themeName);
+    document.body.className = themeName;
+}
+
+export function darkLightSwapper(checkbox: HTMLInputElement): void {
+    if (checkbox.checked) {
+        setTheme('light');
+    } else {
+        setTheme('dark');
+    }
+}
--- news-JS-main/src/index.ts
+++ news-JS-main/src/index.ts
@@ -1,5 +1,11 @@
 import App from './components/app/app';
 import './global.css';
+import { darkLightSwapper } from './components/view/slider';
 
 const app = new App();
 app.start();
+
+const slider = document.getElementById('slider') as HTMLInputElement;
+slider.addEventListener('change', () => {
+    darkLightSwapper(slider);
+});
