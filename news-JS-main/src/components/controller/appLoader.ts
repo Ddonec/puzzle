a6e391770b24d84f191959ec126588cf6bcb0644
Anton <137609245+Ddonec@users.noreply.github.com>
2024-02-26 04:03:36 +0300

    refactor: change appLoader file extension from js to ts and rework types

--- news-JS-main/src/components/controller/appLoader.ts
+++ news-JS-main/src/components/controller/appLoader.ts
@@ -2,10 +2,10 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super(process.env.API_URL, {
-            apiKey: process.env.API_KEY,
+        super(process.env.API_URL as string, {
+            apiKey: process.env.API_KEY as string,
         });
     }
 }
-
+console.log(AppLoader);
 export default AppLoader;
