2e1556f937cd6e96604edb22c9d8ed5cfa3ca8b9
Anton <137609245+Ddonec@users.noreply.github.com>
2024-03-16 02:11:54 +0300

    Feat/rss pz 02 validation (#54)
    * test commit
    
    * husky
    
    * ghfjkld
    
    * feat(config): add dependencies and set config
    
    * feat: rss-pz-000 configuration update (resolve conflict)
    
    * feat(validation): add validation functional for inputs

--- src/components/login-page/button.ts
+++ src/components/login-page/button.ts
@@ -1,6 +1,9 @@
 import './login.css'
-import { BaseComponent, BaseComponentProps } from 'src/types/base-components'
+import { BaseComponent, BaseComponentProps } from 'src/components/basecomponent/base-components'
 
+interface ButtonProps extends BaseComponentProps {
+  type: string
+}
 export class Button extends BaseComponent<HTMLButtonElement> {
   constructor({ className, parent, text, type }: ButtonProps) {
     super({
@@ -13,8 +16,4 @@ export class Button extends BaseComponent<HTMLButtonElement> {
       this.element.type = type
     }
   }
-}
-
-interface ButtonProps extends BaseComponentProps {
-  type: string
 }
